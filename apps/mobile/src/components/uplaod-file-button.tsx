3017db6442b3e25e4f05c1053af609a058008092
Salomon Tshobohwa <stshobohwa@gmail.com>
2026-01-07 14:57:11 +0300

    use proper icons for file type

--- apps/mobile/src/components/uplaod-file-button.tsx
+++ apps/mobile/src/components/uplaod-file-button.tsx
@@ -1,18 +1,37 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function UploadFileButton() {
   return (
     <TouchableOpacity style={styles.container}>
       <View style={styles.view}>
         <Text style={{ fontFamily: 'Outfit-Medium', fontSize: 18 }}>
-          Click to upload a file
+          Click to upload
         </Text>
       </View>
-      <Text style={{ marginTop: 8, fontFamily: 'Outfit-Regular' }}>
-        <Text>📁 PDF</Text>
-        <Text>📄 DOCX</Text>
-        <Text> Power Point</Text>
-      </Text>
+
+      <View style={{ marginTop: 8, flexDirection: 'row', gap: 16 }}>
+        <View style={styles.documentTypeContainer}>
+          <Image
+            source={require('../../assets/images/pdf.png')}
+            style={{ width: 16, height: 16 }}
+          />
+          <Text style={styles.documentTypeText}>PDF</Text>
+        </View>
+        <View style={styles.documentTypeContainer}>
+          <Image
+            source={require('../../assets/images/google-docs.png')}
+            style={{ width: 16, height: 16, marginRight: 16 }}
+          />
+          <Text style={styles.documentTypeText}>DOCX</Text>
+        </View>
+        <View style={styles.documentTypeContainer}>
+          <Image
+            source={require('../../assets/images/powerpoint.png')}
+            style={{ width: 16, height: 16 }}
+          />
+          <Text style={styles.documentTypeText}>Power Point</Text>
+        </View>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -32,5 +51,12 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     borderRadius: 12,
     backgroundColor: '#E0E0E0',
+  },
+  documentTypeContainer: {
+    flexDirection: 'row',
+    gap: 10,
+  },
+  documentTypeText: {
+    fontFamily: 'Outfit-Regular',
   },
 });
