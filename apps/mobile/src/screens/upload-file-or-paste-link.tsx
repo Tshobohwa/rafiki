af23915b16fe0bdd5051caa81e8466fe8234e1b1
Salomon Tshobohwa <stshobohwa@gmail.com>
2026-01-07 17:59:34 +0300

    Add continue button to upload file page

--- apps/mobile/src/screens/upload-file-or-paste-link.tsx
+++ apps/mobile/src/screens/upload-file-or-paste-link.tsx
@@ -1,36 +1,42 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import UploadFileButton from '../components/uplaod-file-button';
 import TextInputPrimary from '../components/text-input-primary';
 import { useState } from 'react';
+import { PrimaryButton } from '../components/primary-button';
 
 export default function UploadFileOrPasteLink() {
   const [link, setLink] = useState('');
   return (
-    <SafeAreaView style={{ flex: 1, padding: 16 }}>
-      <Text style={{ fontFamily: 'Outfit-SemiBold', fontSize: 24 }}>
-        Start a quiz
-      </Text>
-      <Text
-        style={{ fontFamily: 'Outfit-Regular', marginTop: 12, fontSize: 18 }}
-      >
-        Analyze my document to automatically create questions
-      </Text>
-      <UploadFileButton />
-      <Text
-        style={{
-          fontFamily: 'Outfit-Normal',
-          textAlign: 'center',
-          marginVertical: 24,
-        }}
-      >
-        Or
-      </Text>
-      <TextInputPrimary
-        placeholder="Paste a link here"
-        value={link}
-        onChangeText={(link) => setLink(link)}
-      />
+    <SafeAreaView
+      style={{ flex: 1, padding: 24, justifyContent: 'space-between' }}
+    >
+      <View>
+        <Text style={{ fontFamily: 'Outfit-SemiBold', fontSize: 24 }}>
+          Start a quiz
+        </Text>
+        <Text
+          style={{ fontFamily: 'Outfit-Regular', marginTop: 12, fontSize: 18 }}
+        >
+          Analyze my document to automatically create questions
+        </Text>
+        <UploadFileButton />
+        <Text
+          style={{
+            fontFamily: 'Outfit-Normal',
+            textAlign: 'center',
+            marginVertical: 24,
+          }}
+        >
+          Or
+        </Text>
+        <TextInputPrimary
+          placeholder="Paste a link here"
+          value={link}
+          onChangeText={(link) => setLink(link)}
+        />
+      </View>
+      <PrimaryButton text="Continue" />
     </SafeAreaView>
   );
 }
