64f8ff66140d6845ead710d6626986409de33184
Tshobohwa <stshobohwa@gmail.com>
2026-01-23 16:14:46 +0300

    Update side bar with buttons

--- web/components/app-sidebar.tsx
+++ web/components/app-sidebar.tsx
@@ -5,16 +5,31 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar";
+import { Button } from "./ui/button";
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        <div className="">Rafiki</div>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
+        <SidebarGroup className="flex flex-col gap-4">
+          <Button className="h-12 flex justify-start px-6" variant={"default"}>
+            Start a quiz
+          </Button>
+          <Button className="h-12 flex justify-start px-6" variant={"default"}>
+            Start a quiz
+          </Button>
+          <Button className="h-12 flex justify-start px-6" variant={"default"}>
+            Start a quiz
+          </Button>
+        </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <div>Footer Content</div>
+      </SidebarFooter>
     </Sidebar>
   );
 }
