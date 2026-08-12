a718cf8451b0ba2b944b407da0b8fe5e3a9865ca
Tshobohwa <stshobohwa@gmail.com>
2026-08-13 01:46:29 +0200

    Create login page

---
+++ mobile/components/auth-method-button.tsx
@@ -0,0 +1,33 @@
+import { Image, Text, TouchableOpacity } from "react-native";
+
+export default function AuthMethodButton({
+  image,
+  onPress,
+  text = "",
+  children,
+}: {
+  icon: React.ReactNode;
+  image?: string;
+  onPress: () => void;
+  text: string;
+  children?: React.ReactNode;
+}) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        padding: 10,
+        borderRadius: 55 / 2,
+        height: 55,
+      }}
+    >
+      {children}
+      <Text style={{ fontFamily: "Outfit-Regular", fontSize: 18 }}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
--- mobile/screens/login.tsx
+++ mobile/screens/login.tsx
@@ -1,12 +1,19 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import TextInputPrimary from "@/components/text-input-primary";
 import { useState } from "react";
+import { PrimaryButton } from "@/components/primary-button";
+import AuthMethodButton from "@/components/auth-method-button";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const submit = () => {
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
   return (
     <SafeAreaView style={{ flex: 1, padding: 20, gap: 25 }}>
       <TouchableOpacity>
@@ -40,6 +47,24 @@ export default function LoginScreen() {
           Forgot password?
         </Text>
       </TouchableOpacity>
+      <PrimaryButton text="Login" />
+      <Text
+        style={{
+          fontFamily: "Outfit-Regular",
+          fontSize: 16,
+          textAlign: "center",
+        }}
+      >
+        Or login with
+      </Text>
+      <View>
+        <AuthMethodButton text="Google" onPress={() => submit()}>
+          <Image
+            source={require("../assets/images/google.png")}
+            style={{ width: 28, height: 28 }}
+          />
+        </AuthMethodButton>
+      </View>
       <TouchableOpacity>
         <Text style={{ fontFamily: "Outfit-Regular", fontSize: 16 }}>
           Don't have an account?{" "}
